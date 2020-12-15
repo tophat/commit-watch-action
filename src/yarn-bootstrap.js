430ee5ad98389a2d7601c3d1a604def89fdb8e7a
Noah <noah.negin-ulster@tophatmonocle.com>
2020-12-15 18:59:45 -0500

    feat: initial version (#1)

---
+++ src/yarn-bootstrap.js
@@ -0,0 +1,5 @@
+const { execSync } = require('child_process')
+const path = require('path')
+const index = path.resolve(__dirname, 'index.js')
+
+execSync(`yarn node ${index}`, { stdio: 'inherit' })
