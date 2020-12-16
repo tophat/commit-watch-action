0a568840bb9614bdf3e7e678e6557f37fd0ffa8d
Noah Negin-Ulster <noah.negin-ulster@tophatmonocle.com>
2020-12-16 11:24:41 -0500

    wip: change to inherit

--- src/yarn-bootstrap.js
+++ src/yarn-bootstrap.js
@@ -3,7 +3,7 @@ const path = require('path')
 const index = path.resolve(__dirname, 'index.js')
 
 try {
-    execSync(`yarn node ${index}`, { stdio: 'ignore' })
+    execSync(`yarn node ${index}`, { stdio: 'inherit' })
 } catch (err) {
     console.error(err)
     console.error('Failed to run Commit Watch Action. Please open an issue: https://github.com/tophat/commit-watch-action/issues/new?template=bug_report.md.')
