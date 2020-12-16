2c6310ad0202bd59ca637fdac347d06a0cfaf018
Noah Negin-Ulster <noah.negin-ulster@tophatmonocle.com>
2020-12-16 12:11:54 -0500

    fix: specify cwd dir

--- src/yarn-bootstrap.js
+++ src/yarn-bootstrap.js
@@ -1,9 +1,10 @@
 const { execSync } = require('child_process')
 const path = require('path')
 const index = path.resolve(__dirname, 'index.js')
+const rootDir = path.resolve(__dirname, '..')
 
 try {
-    execSync(`yarn node ${index}`, { stdio: 'inherit' })
+    execSync(`yarn node ${index}`, { stdio: 'inherit', cwd: rootDir })
 } catch (err) {
     console.error(err)
     console.error('Failed to run Commit Watch Action. Please open an issue: https://github.com/tophat/commit-watch-action/issues/new?template=bug_report.md.')
